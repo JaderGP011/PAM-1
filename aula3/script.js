14d244da9705700b234e4d63a6e7951485d8d142
JaderGP011 <jadergamaperes@gmail.com>
2024-10-16 07:51:34 -0300

    ainda incompleto

--- aula3/script.js
+++ aula3/script.js
@@ -4,27 +4,43 @@ function enter() {
   switch (valor) {
     case 3:
       document.getElementById("img").src = "./images/triangulo.jpeg";
+      document.getElementById("perimetro").innerText = "Perímetro: P=3⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 4:
       document.getElementById("img").src = "./images/quadrado.png";
+      document.getElementById("perimetro").innerText = "Perimetro: P=4⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 5:
       document.getElementById("img").src = "./images/pentagono.png";
+      document.getElementById("perimetro").innerText = "Perimetro: P=5⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 6:
       document.getElementById("img").src = "./images/hexagono.jpg";
+      document.getElementById("perimetro").innerText = "Perimetro: P=6⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 7:
       document.getElementById("img").src = "./images/heptagono.png";
+      document.getElementById("perimetro").innerText = "Perimetro: P=7⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 8:
       document.getElementById("img").src = "./images/octogono.png";
+      document.getElementById("perimetro").innerText = "Perimetro: P=8⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 9:
       document.getElementById("img").src = "./images/nonagono.png";
+      document.getElementById("perimetro").innerText = "Perimetro: P=9⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     case 10:
       document.getElementById("img").src = "./images/decagono.jpg";
+      document.getElementById("perimetro").innerText = "Perimetro: P=10⋅l"
+      document.getElementById("area").innerText = "Area: "
       break;
     default:
       alert("Número não catalogado ou não existente");
